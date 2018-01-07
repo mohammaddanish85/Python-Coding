137eee2b4d12dcf215fc269ed3e1554918fa10cf
mohammaddanish85 <35197775+mohammaddanish85@users.noreply.github.com>
2018-01-07 23:17:23 +0530

    Add files via upload

---
+++ Addition of Two Numbers.py
@@ -0,0 +1,12 @@
+# This program demonstration of addition of two numbers by initialising the variable value.
+
+""" This type of comment method is used to create API documentation file. This is called multi line comment. But do
+not use this method as these statement occupy space in memory and interpreter will read them and it would result in
+cost of time."""
+
+# If you want to read the API document for this file then type    -m file_name -w ex   and execute the program. A file_name.html would be created and you can look at the output.
+
+a=10                              # Value 10 is assigned to variable a. This a is usually called as tag for value 10.
+b=5                               # Value 5 is assigned to variable b . This b is usually called as tag for value 5.
+c=a+b                             # Variable c consists of value after adding the value of a and b variable.
+print("Addition is =", c)         # Statement print the value of c variable.
