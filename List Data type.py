84952f26a01f917f0fb45ee332d19459ddff9fbd
mohammaddanish85 <35197775+mohammaddanish85@users.noreply.github.com>
2018-01-08 22:10:28 +0530

    Add files via upload

---
+++ List Data type.py
@@ -0,0 +1,12 @@
+# This program demonstrate the concept of list in python.
+# Lsi is similar to array in C/Java. List represent the group of elements.
+# Difference between list and array is that List can store different types of elements while array can store similar type of elements.
+
+
+list=[2, 'Danish', 23.5, 'Ariba']                 # Create a list having different elements.
+
+print(list)                                       # Print the entire list.
+
+print(list[2])                                    # Print the 2nd element in list
+
+print(list[1:3])                                  # Print the elements from 1st to 3rd position in list.
