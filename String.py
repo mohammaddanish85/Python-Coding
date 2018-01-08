590b4d412a74b75bf1db5aa8203f2ca13426b245
mohammaddanish85 <35197775+mohammaddanish85@users.noreply.github.com>
2018-01-08 22:06:58 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,18 @@
+# This Program demonstrate the use of String data type.
+
+
+str= "Welcome to the World of Python"                                     # In this, statement can be stored in double quotes.
+str1= 'Welcome to World of Python (2nd type)'                             # In this, statement can be stored in single quotes.
+str2= """ This is an "example of printing statement" in double quote"""
+
+print(str)                                                                # Display the output.
+
+print(str1)                                                               # Display the output.
+
+print(str2)                                                               # Display the output.
+
+print(str[0])                                                             # Display the first character of string.
+
+print(str[3:10])                                                          # Display the characters from 3rd element to 10th element.
+
+
