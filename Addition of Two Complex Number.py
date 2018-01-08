ab60d507e92d2c87e7998c42671368e4c4d05d6e
mohammaddanish85 <35197775+mohammaddanish85@users.noreply.github.com>
2018-01-08 17:18:38 +0530

    Add files via upload

---
+++ Addition of Two Complex Number.py
@@ -0,0 +1,11 @@
+# Python program to add two complex numbers
+
+# This program demonstrates the addition of two complex numbers.
+
+
+c1= 2.5 + 3.0j                               # Value of first complex number
+c2= 3.3 + 2.1j                               # Valus if second complex number
+c3= c1+c2                                    # Sum of two complex number stored in third variable.
+print("Addition of two numbers is=", c3)     # Display the result.
+
+
