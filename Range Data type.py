5e83b4bc6c792ab109398da56dc465be3084d772
mohammaddanish85 <35197775+mohammaddanish85@users.noreply.github.com>
2018-01-08 22:10:06 +0530

    Add files via upload

---
+++ Range Data type.py
@@ -0,0 +1,10 @@
+# Program for Range Data type.
+# Range data type represents sequence of numbers. Numbers in range cannot be modified.
+
+
+
+r= range(10)                        # Create range number
+for i in r: print(i)                # Print values from 0 to 9 (Total 10 numbers)
+
+r= range(2, 22 , 2)                 # Here range is starting from 2 and limit is 22. There will be an increment of 2 (Look at Last number).
+for i in r: print(i)                # Print the values starting from 2 and upto 20. (Total 10 values)
