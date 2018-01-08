f1f9e0a219b2b73e2cafb2ce9fbab361ef3b4481
mohammaddanish85 <35197775+mohammaddanish85@users.noreply.github.com>
2018-01-08 22:09:44 +0530

    Add files via upload

---
+++ Tuple Data Type.py
@@ -0,0 +1,11 @@
+# Tuple is similar to list.
+# A Tuple can have different type of elements.
+# Tuple have () parenthesis.
+# Unlike list, tuple elements cannot be modified.
+
+
+tpl= (10, 22.5, 'Danish', 'Sahil' )             # Elements of Tuple
+
+print(tpl[0])                                   # Print 0th value of tuple.
+
+print(tpl[1:3])                                 # Print 2nd & 3rd element of tuple.
