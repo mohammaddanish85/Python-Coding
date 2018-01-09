10cf946fb92db00888747ee0b53438e5903005c2
Mohammad Danish <mohammaddanish85@gmail.com>
2018-01-09 11:03:39 +0530

    Add files via upload

---
+++ Set Datatype.py
@@ -0,0 +1,9 @@
+# A Set is an unordered collection of elements.
+# Order of elements is not maintained in sets.
+# Set does not accept duplicate elements.
+
+s={23, 35, 61, 88, 99}      # Set consists of 5 elements.
+print(s)                    # Printing the elements of set.
+
+ch=set("Hello")             # Storing the elementsof sets in a variable.
+print(ch)                   # Printing the elements of set.
