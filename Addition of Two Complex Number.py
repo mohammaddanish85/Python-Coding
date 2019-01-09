50ab3af30289d0237cb4b0f094fb90b60cb831ae
Mohammad Danish <mohammaddanish85@gmail.com>
2019-01-09 22:08:49 +0530

    Update Addition of Two Complex Number.py

--- Addition of Two Complex Number.py
+++ Addition of Two Complex Number.py
@@ -4,6 +4,6 @@
 
 
 c1= 2.5 + 3.0j                               # Value of first complex number
-c2= 3.3 + 2.1j                               # Valus of second complex number
+c2= 3.3 + 2.1j                               # Value of second complex number
 c3= c1+c2                                    # Sum of two complex number stored in third variable.
 print("Addition of two numbers is=", c3)     # Display the result.
